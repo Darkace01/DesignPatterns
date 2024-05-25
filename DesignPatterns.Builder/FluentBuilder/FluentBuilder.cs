da319a182499eb3a6cd584b6b56ee00b401c9597
Kazeem Quadri <quadrikazeem01@gmail.com>
2024-05-25 12:54:37 +0100

    Builder and FluentBuilder
    The most significant changes include the addition of a new project to the Visual Studio solution file, the creation of new files that demonstrate the use of StringBuilder and the implementation of a fluent interface in the HtmlBuilder class.
    
    1. A new project named "DesignPatterns.Builder" has been added to the Visual Studio solution file "DesignPatterns.sln". This project uses the .NET 8.0 framework and is configured to output an executable file. The project file also enables implicit usings and nullable reference types.
    
    2. A new file "GlobalUsing.cs" has been added. This file contains global using directives for `System.Console` and `System.Text`.
    
    3. A new file "Program.cs" has been added. This file contains a simple console application that demonstrates the use of a StringBuilder to create HTML elements. It also introduces a new class `HtmlElement` and `HtmlBuilder` which are used to build HTML elements in a structured way.
    
    4. A new file "FluentBuilder.cs" has been added in the "DesignPatterns.Builder.FluentBuilder" namespace. This file contains a static class `FluentBuilder` with a `Run` method that demonstrates the use of the `HtmlBuilder` class to create HTML elements. The `HtmlBuilder` class in this file has been modified to return itself in the `AddChild` method, allowing for a fluent interface. The `HtmlElement` class is also included in this file, and is identical to the one in "Program.cs".

---
+++ DesignPatterns.Builder/FluentBuilder/FluentBuilder.cs
@@ -0,0 +1,100 @@
+﻿namespace DesignPatterns.Builder.FluentBuilder;
+public static class FluentBuilder
+{
+    public static void Run()
+    {
+        Console.WriteLine("Hello, World!");
+        var hello = "hello";
+        var sb = new StringBuilder();
+        sb.Append("<p>");
+        sb.Append(hello);
+        sb.Append("</p>");
+        Console.WriteLine(sb);
+
+        var words = new[] { "hello", "world" };
+        sb.Clear();
+        sb.Append("<ul>");
+        foreach (var word in words)
+        {
+            sb.AppendFormat("<li>{0}</li>", word);
+        }
+        sb.Append("</ul>");
+        Console.WriteLine(sb);
+
+        var builder = new HtmlBuilder("ul");
+        // Fluent Interface
+        builder.AddChild("li", "hello").AddChild("li", "world");
+        Console.WriteLine(builder);
+    }
+}
+
+public class HtmlElement
+{
+    public string Name, Text;
+    public List<HtmlElement> Elements = [];
+    private const int indentSize = 2;
+    public HtmlElement()
+    {
+
+    }
+    public HtmlElement(string name, string text)
+    {
+        Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+        Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
+    }
+
+    private string ToStringImpl(int indent)
+    {
+        var sb = new StringBuilder();
+        var i = new string(' ', indentSize * indent);
+        sb.Append($"{i}<{Name}>\n");
+
+        if (!string.IsNullOrWhiteSpace(Text))
+        {
+            sb.Append(new string(' ', indentSize * (indent + 1)));
+            sb.Append(Text);
+            sb.Append('\n');
+        }
+
+        foreach (var e in Elements)
+        {
+            sb.Append(e.ToStringImpl(indent + 1));
+        }
+
+        sb.Append($"{i}</{Name}>\n");
+        return sb.ToString();
+    }
+    public override string ToString()
+    {
+        return ToStringImpl(0);
+    }
+}
+
+public class HtmlBuilder
+{
+    private readonly string rootName;
+    HtmlElement root = new();
+
+    public HtmlBuilder(string rootName)
+    {
+        this.rootName = rootName;
+        root.Name = rootName;
+    }
+
+    public HtmlBuilder AddChild(string childName, string childText)
+    {
+        var e = new HtmlElement(childName, childText);
+        root.Elements.Add(e);
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return root.ToString();
+    }
+
+    public void Clear()
+    {
+        root = new HtmlElement { Name = rootName };
+    }
+}
